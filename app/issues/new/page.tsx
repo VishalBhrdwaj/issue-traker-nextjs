7ba46886ff58b3520dbb98ed7c311a5942619a26
Vishal Bhardwaj <vishalbhardwaj@Vishals-MacBook-Air.local>
2023-10-14 17:05:09 +0530

    show all issues on issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,35 +3,57 @@
 import SimpleMDE from "react-simplemde-editor";
 import {useForm,Controller} from 'react-hook-form'
 import "easymde/dist/easymde.min.css";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import z from "zod";
+import Spinner from "@/app/components/Spinner";
 
-interface IssueForm {
-  title:string;
-  description :string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
-  const {register,control,handleSubmit}=useForm<IssueForm>();
+  const {register,control,handleSubmit,formState:{errors}}=useForm<IssueForm>({
+    resolver:zodResolver(createIssueSchema)
+  });
   const router=useRouter();
+  const [error,setError]=useState('');
+  const [isSubmitting,setSubmitting]=useState(false);
   return (
+    <div className="max-w-xl">
+      {error &&<Callout.Root color="red" className="mb-5">
+        <Callout.Text>
+          {error}
+        </Callout.Text>
+        </Callout.Root>}
     <form onSubmit={handleSubmit(async(data)=>{
-      await axios.post('/api/issues',data);
-      router.push('/issues')
+      try {
+        setSubmitting(true);
+        await axios.post('/api/issues',data);
+        router.push('/issues')
+      
+      } catch (error) {
+        setSubmitting(false)
+        setError('An Unexpected error');
+      }
 
-    })} className="max-w-xl space-y-3">
+    })} className=" space-y-3">
       <TextField.Root>
         <TextField.Input placeholder="Title" {...register('title')}/>
       </TextField.Root>
+      {errors.title && <Text as="p" color="red">{errors.title.message}</Text>}
       <Controller
         name="description"
         control={control}
         render={({field})=> <SimpleMDE placeholder="Description" {...field} />}
         />
-      <Button>Submit New Issue</Button>
+        {errors.description && <Text as="p" color="red">{errors.description.message}</Text>}
+      <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner/>}</Button>
      
     </form>
+    </div>
   );
 };
 
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,36 @@
 
 import React from 'react'
-import { Button } from '@radix-ui/themes'
+import { Button, Table, TableBody, TableCell, TableColumnHeaderCell, TableHeader, TableRoot, TableRow } from '@radix-ui/themes'
 import Link from 'next/link'
+import prisma from '@/prisma/client'
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues=await prisma.issues.findMany();
   return (
     <div>
-      
+      <div className='mb-5'>
       <Button><Link href='/issues/new'>New Issue</Link></Button>
+      </div>
+      <TableRoot variant='surface'>
+          <TableHeader>
+            <TableRow>
+            <TableColumnHeaderCell>Id</TableColumnHeaderCell>
+            <TableColumnHeaderCell className='hidden md:table-cell'>Title</TableColumnHeaderCell>
+            <TableColumnHeaderCell className='hidden md:table-cell'>Description</TableColumnHeaderCell>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {
+              issues.map( issue =>(
+                <TableRow key={issue.id}>
+                  <TableCell>{issue.title}</TableCell>
+                  <TableCell className='hidden md:table-cell'>{issue.status}</TableCell>
+                  <TableCell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</TableCell>
+                </TableRow>
+              ))
+            }
+          </TableBody>
+      </TableRoot>
     </div>
   )
 }
